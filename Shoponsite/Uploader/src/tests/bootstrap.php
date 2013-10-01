dadb38eb12b994064d1d45bf02d27234abcecf77
Thomas Warlop <thomas.warlop@gmail.com>
2013-10-01 13:51:37 +0200

    moved composer file + setup bootstrap file correctly

--- Shoponsite/Uploader/src/tests/bootstrap.php
+++ Shoponsite/Uploader/src/tests/bootstrap.php
@@ -1,15 +1,2 @@
 <?php
-spl_autoload_register(function($className){
-    $base = __DIR__ . '/../';
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strripos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-    require ($base . $fileName);
-});
+include(__DIR__ . '/../vendor/autoload.php');
